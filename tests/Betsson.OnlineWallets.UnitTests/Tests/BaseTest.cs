d6427d7e287540dbfef50ad140eabaa6603b0c20
martochab <martulonco@hotmail.com>
2025-11-20 01:50:17 -0300

    Created folders and main files of Unit Test Project

---
+++ tests/Betsson.OnlineWallets.UnitTests/Tests/BaseTest.cs
@@ -0,0 +1,24 @@
+﻿using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Betsson.OnlineWallets.UnitTests.Tests
+{
+    public class BaseTest
+    {
+        [SetUp]
+        public void Setup()
+        {
+
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+
+        }
+    }
+}
