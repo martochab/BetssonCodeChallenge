6241586cc17670093efc9243ba9c4611e3bdcb9b
martochab <martulonco@hotmail.com>
2025-11-21 15:20:55 -0300

    Created deposite test cases T008,T009,T010

--- tests/Betsson.OnlineWallets.ApiTests/Tasks/DepositTasks.cs
+++ tests/Betsson.OnlineWallets.ApiTests/Tasks/DepositTasks.cs
@@ -1,12 +1,28 @@
-﻿using System;
+﻿using Betsson.OnlineWallets.Models;
+using Betsson.OnlineWallets.Web.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
+using Betsson.OnlineWallets.ApiTests.Constants;
+using Betsson.OnlineWallets.ApiTests.Helpers;
 
 namespace Betsson.OnlineWallets.ApiTests.Tasks
 {
     internal class DepositTasks
     {
+        public static async Task<HttpResponseMessage> PostDepositAsync(ApiClient client, DepositRequest deposit)
+        {
+            var response = await client.PostRawAsync(EndPoints.UrlDeposit, deposit);
+            return response;
+        }
+
+        public static async Task<BalanceResponse> PostDepositGetBalanceAsync(ApiClient client, DepositRequest deposit)
+        {
+            var response = await client.PostAsync<BalanceResponse>(EndPoints.UrlDeposit,deposit);
+            return response;
+        }
     }
 }
--- tests/Betsson.OnlineWallets.ApiTests/Tests/DepositTests.cs
+++ tests/Betsson.OnlineWallets.ApiTests/Tests/DepositTests.cs
@@ -1,13 +1,47 @@
-﻿using System;
+﻿using Betsson.OnlineWallets.ApiTests.Tasks;
+using Betsson.OnlineWallets.Models;
+using Betsson.OnlineWallets.Web.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Betsson.OnlineWallets.ApiTests.Tests
 {
-    internal class DepositTests
+    internal class DepositTests : BaseTest
     {
+        [Test]
+        public async Task T008DepositShouldIncreaseBalance()
+        {
+            var initial = await BalanceTasks.GetBalanceAsync(_client);
 
+            var deposit = new DepositRequest { Amount = 100 };
+
+            var response =await DepositTasks.PostDepositGetBalanceAsync(_client, deposit);
+
+            Assert.That(response.Amount, Is.EqualTo(initial.Amount + 100));
+        }
+
+        [Test]
+        public async Task T009DepositShouldReturn400WhenAmountIsNegative()
+        {
+            var deposit = new DepositRequest { Amount = -50 };
+
+            var response = await DepositTasks.PostDepositAsync(_client, deposit);
+
+            Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.BadRequest));
+        }
+
+        [Test]
+        public async Task T010DepositShouldReturn400WhenBodyIsEmpty()
+        {
+            DepositRequest deposit = null;
+
+            var response = await DepositTasks.PostDepositAsync(_client,deposit);
+
+            Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.BadRequest));
+        }
     }
 }
