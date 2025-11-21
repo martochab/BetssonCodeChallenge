c7c7ef1ff23faaf4c4ea4d532ec31a479e2ed264
martochab <martulonco@hotmail.com>
2025-11-21 16:40:51 -0300

    created Withdrawal test cases T011,T012,T013,T014

--- tests/Betsson.OnlineWallets.ApiTests/Tasks/WithdrawTasks.cs
+++ tests/Betsson.OnlineWallets.ApiTests/Tasks/WithdrawTasks.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Betsson.OnlineWallets.ApiTests.Constants;
+using Betsson.OnlineWallets.ApiTests.Helpers;
+using Betsson.OnlineWallets.Web.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +11,17 @@ namespace Betsson.OnlineWallets.ApiTests.Tasks
 {
     internal class WithdrawTasks
     {
+        public static async Task<HttpResponseMessage> PostWithdrawAsync(ApiClient client, WithdrawalRequest deposit)
+        {
+            var response = await client.PostRawAsync(EndPoints.UrlWithdraw, deposit);
+            return response;
+        }
+
+
+        public static async Task<BalanceResponse> PostWithdrawGetBalanceAsync(ApiClient client, WithdrawalRequest deposit)
+        {
+            var response = await client.PostAsync<BalanceResponse>(EndPoints.UrlWithdraw, deposit);
+            return response;
+        }
     }
 }
--- tests/Betsson.OnlineWallets.ApiTests/Tests/WithdrawTests.cs
+++ tests/Betsson.OnlineWallets.ApiTests/Tests/WithdrawTests.cs
@@ -1,13 +1,61 @@
-﻿using System;
+﻿using Betsson.OnlineWallets.ApiTests.Tasks;
+using Betsson.OnlineWallets.Models;
+using Betsson.OnlineWallets.Web.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Betsson.OnlineWallets.ApiTests.Tests
 {
-    internal class WithdrawTests
+    internal class WithdrawTests : BaseTest
     {
+        [Test]
+        public async Task T011WithdrawShouldReduceBalance()
+        {
+            var deposit=new DepositRequest { Amount = 100 };
 
+            await DepositTasks.PostDepositGetBalanceAsync(_client, deposit);
+
+            var before = await BalanceTasks.GetBalanceAsync(_client);
+
+            var request = new WithdrawalRequest { Amount = 50 };
+
+            var response = await WithdrawTasks.PostWithdrawGetBalanceAsync(_client, request);
+
+            Assert.That(response.Amount, Is.EqualTo(before.Amount - 50));
+        }
+
+        [Test]
+        public async Task T012WithdrawShouldReturn400WhenAmountIsNegative()
+        {
+            var request = new WithdrawalRequest { Amount = -20 };
+
+            var response = await WithdrawTasks.PostWithdrawAsync(_client, request);
+
+            Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.BadRequest));
+        }
+
+        [Test]
+        public async Task T013WithdrawShouldReturn400WhenInsufficientBalance()
+        {
+            var request= new WithdrawalRequest { Amount = 9999999999};
+
+            var response = await WithdrawTasks.PostWithdrawAsync(_client, request);
+
+            Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.BadRequest));
+        }
+
+        [Test]
+        public async Task T014WithdrawShouldReturn400WhenBodyIsEmpty()
+        {
+            WithdrawalRequest request = null;
+
+            var response = await WithdrawTasks.PostWithdrawAsync(_client, request);
+
+            Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.BadRequest));
+        }
     }
 }
